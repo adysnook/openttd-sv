80e6ab3853b3218ffb028b2bbfc927326711ba0c
Adrian Munteanu <munty92@gmail.com>
2026-08-17 19:33:07 +0000

    fix docker

--- bot/bot.py
+++ bot/bot.py
@@ -1,7 +1,11 @@
 import time
 import json
 import threading
+import socket
 from pyopenttdadmin import Admin, AdminUpdateType, openttdpacket as p, Auth
+
+print("[Bot     ] Starting bot...")
+
 
 class AdminGameScriptPacket(p.Packet):
     # Tipul pachetului pentru direcția Bot -> GameScript
@@ -39,66 +43,81 @@ def send_to_gamescript(self, data):
 Admin.send_gamescript = send_to_gamescript
 
 
+
 password = "parola_sv"
 ip_address = "openttd-server"
 port_number = 3977
+timeout_total = 60
+    
+auth = Auth(name="MUNTY-Bot", version="15.0", password=password)
+bot = None
+start_time = time.time()
+while True:
+    if time.time() - start_time > timeout_total:
+        print("[Bot     ] Connection timed out")
+        exit(1)
+        
+    try:
+        print(f"[Bot     ] Connecting to OpenTTD server at ({ip_address}:{port_number})")
+        bot = Admin(ip=ip_address, port=port_number, auth=auth)
 
-auth = Auth(
-    name="MUNTY-Bot",
-    version="15.0",
-    password=password,
-)
-
-bot = Admin(ip=ip_address, port=port_number, auth=auth)
-
-bot.subscribe(AdminUpdateType.CLIENT_INFO)
-bot.subscribe(AdminUpdateType.CHAT)
-bot.subscribe(AdminUpdateType.CONSOLE)
-bot.subscribe(AdminUpdateType.CMD_LOGGING)
-bot.subscribe(AdminUpdateType.GAMESCRIPT)
+        bot.subscribe(AdminUpdateType.CLIENT_INFO)
+        bot.subscribe(AdminUpdateType.CHAT)
+        bot.subscribe(AdminUpdateType.CONSOLE)
+        bot.subscribe(AdminUpdateType.CMD_LOGGING)
+        bot.subscribe(AdminUpdateType.GAMESCRIPT)
 
 
-@bot.add_handler(p.ChatPacket)
-def chat_packet(bot: Admin, packet: p.ChatPacket):
-    print(f'[Chat    ] {packet.id}: {packet.message} ({packet.action})')
+        @bot.add_handler(p.ChatPacket)
+        def chat_packet(bot: Admin, packet: p.ChatPacket):
+            print(f'[Chat    ] {packet.id}: {packet.message} ({packet.action})')
 
-@bot.add_handler(p.AdminJoinPacket)
-def admin_join(bot: Admin, packet: p.AdminJoinPacket):
-    print(f"[Admin   ] Admin {packet.name} connnected with version {packet.version}.")
+        @bot.add_handler(p.AdminJoinPacket)
+        def admin_join(bot: Admin, packet: p.AdminJoinPacket):
+            print(f"[Admin   ] Admin {packet.name} connnected with version {packet.version}.")
 
-@bot.add_handler(p.WelcomePacket)
-def welcome(bot: Admin, packet: p.WelcomePacket):
-    print(f"[Admin   ] Welcome from {packet.server_name}!")
+        @bot.add_handler(p.WelcomePacket)
+        def welcome(bot: Admin, packet: p.WelcomePacket):
+            print(f"[Admin   ] Welcome from {packet.server_name}!")
 
-@bot.add_handler(p.ErrorPacket)
-def error(bot: Admin, packet: p.ErrorPacket):
-    print(f"[Error   ] {packet.message}")
+        @bot.add_handler(p.ErrorPacket)
+        def error(bot: Admin, packet: p.ErrorPacket):
+            print(f"[Error   ] {packet.message}")
 
-@bot.add_handler(p.GameScriptPacket)
-def gamescript_packet(bot: Admin, packet: p.GameScriptPacket):
-    print(f"[Script  ] Update ({len(packet.json)} bytes): {packet.json}")
-    data = json.loads(packet.json[:-1])
-    if data.get("type") == "restart_map":
-        bot.send_rcon("restart")
-    if data.get("type") == "send_global":
-        bot.send_global(data.get("message", ""))
-    if data.get("type") == "send_private":
-        player_id = data.get("player_id")
-        message = data.get("message", "")
-        if player_id is not None:
-            bot.send_private(message, player_id)
+        @bot.add_handler(p.GameScriptPacket)
+        def gamescript_packet(bot: Admin, packet: p.GameScriptPacket):
+            print(f"[Script  ] Update ({len(packet.json)} bytes): {packet.json}")
+            data = json.loads(packet.json[:-1])
+            if data.get("type") == "restart_map":
+                bot.send_rcon("restart")
+            if data.get("type") == "send_global":
+                bot.send_global(data.get("message", ""))
+            if data.get("type") == "send_private":
+                player_id = data.get("player_id")
+                message = data.get("message", "")
+                if player_id is not None:
+                    bot.send_private(message, player_id)
 
-@bot.add_handler(p.ConsolePacket)
-def console_packet(bot: Admin, packet: p.ConsolePacket):
-    print(f"[Console ] {packet.origin}: {packet.message}")
+        @bot.add_handler(p.ConsolePacket)
+        def console_packet(bot: Admin, packet: p.ConsolePacket):
+            print(f"[Console ] {packet.origin}: {packet.message}")
 
-@bot.add_handler(p.ClientJoinPacket)
-def client_join(bot: Admin, packet: p.ClientJoinPacket):
-    print(f"[Client  ] Client {packet.id} joined the game.")
-    bot.send_gamescript({
-        "type": "player_joined",
-        "player_id": packet.id
-    })
+        @bot.add_handler(p.ClientJoinPacket)
+        def client_join(bot: Admin, packet: p.ClientJoinPacket):
+            print(f"[Client  ] Client {packet.id} joined the game.")
+            bot.send_gamescript({
+                "type": "player_joined",
+                "player_id": packet.id
+            })
 
-print("[Bot] Starting bot...")
-bot.run()
+        bot.run()
+        print("[Bot     ] End")
+        
+    except Exception as e:
+        print(f"[Bot     ] Connection timed out. {e}")
+        
+    print("[Bot     ] Waiting 5 seconds")
+    time.sleep(5)
+
+print("[Bot     ] Connected")
+
